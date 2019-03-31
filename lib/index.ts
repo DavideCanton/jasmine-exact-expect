6f314668858856178e0e1c39811867612ced4e8e
Davide Canton <davide.canton5@gmail.com>
2019-03-31 17:41:42 +0200

    ignored useless files

--- lib/index.ts
+++ lib/index.ts
@@ -1,34 +1,55 @@
 import * as _ from 'lodash';
 
-if(!_.isFunction(beforeEach) || !_.isFunction(afterEach) || !_.isFunction(expect))
-    throw new Error('jasmine globals not found.');
+(function(globalObj)
+{
+    if(!_.isFunction(globalObj.beforeEach) || !_.isFunction(globalObj.afterEach) || !_.isFunction(globalObj.expect) || !_.isFunction(globalObj.expectAsync))
+        throw new Error('jasmine globals not found.');
+})(this);
 
-let expectedExpects = null;
-let spy: jasmine.Spy;
+let expectedExpects: number | null = null;
+let expectedAsyncExpects: number | null = null;
+let expectSpy: jasmine.Spy;
+let expectAsyncSpy: jasmine.Spy;
 
 export function expectCount(num: string | number)
 {
-    num = _.isString(num) ? parseInt(num, 10) : num;
-
-    if(isNaN(num) || num < 0)
-        throw new Error('jasmine.expectCount expects a number >= 0 as the first argument.');
-
+    num = parseNumber(num);
     expectedExpects = num;
 }
 
-function throwError(expected: number, actual: number)
+export function expectAsyncCount(num: string | number)
+{
+    num = parseNumber(num);
+    expectedAsyncExpects = num;
+}
+
+function parseNumber(num: string | number)
+{
+    num = _.isString(num) ? parseInt(num, 10) : num;
+    if(isNaN(num) || num < 0)
+        throw new Error('expects a number >= 0 as the first argument.');
+    return num;
+}
+
+function throwError(expected: number | null, actual: number)
 {
     if(expected !== null && actual !== expected)
         throw new Error(`Expected ${expected} expect${expected !== 1 ? 's' : ''} to be called, ${actual} expect${actual !== 1 ? 's were' : ' was'} actually called.`);
 }
 
-beforeEach(function()
+(function(globalObj)
 {
-    expectedExpects = null;
-    spy = spyOn(global as any, 'expect').and.callThrough();
-});
+    beforeEach(function()
+    {
+        expectedExpects = null;
+        expectedAsyncExpects = null;
+        expectSpy = spyOn(globalObj as any, 'expect').and.callThrough();
+        expectAsyncSpy = spyOn(globalObj as any, 'expectAsync').and.callThrough();
+    });
 
-afterEach(function()
-{
-    throwError(expectedExpects, spy.calls.count());
-});
+    afterEach(function()
+    {
+        throwError(expectedExpects, expectSpy.calls.count());
+        throwError(expectedAsyncExpects, expectAsyncSpy.calls.count());
+    });
+})(this);
